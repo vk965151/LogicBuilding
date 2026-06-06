65e85bca67e002bb05fce5546f6807b2c1b610a2
Vishal Kashyap <vk965151@gmail.com>
2026-06-06 13:09:57 +0530

    Added while loop programs

---
+++ Whileloop/table2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 2;
+    int i=1;
+    int cross;
+    while(i<=10){
+        cout<<num<<" * "<<i<<" = "<<num*i<<endl;
+        i++;
+    }
+
+
+    return 0;
+}
